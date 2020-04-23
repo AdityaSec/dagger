ac15dcca820778b1293a57bfa624739dab758923
D3v1LaL <40142550+AdityaSec@users.noreply.github.com>
2020-04-23 09:16:26 +0530

    Update dagger.py
    Removed No Longer Used Lines.

--- dagger.py
+++ dagger.py
@@ -473,27 +473,6 @@ def vigenere(enc,key):
 	    alph = 'abcdefghijklmnopqrstuvwxyz'
 	    new_alph = alph[alph.index(ch):] + alph[:alph.index(ch)]
 	    return new_alph
-	def encrypt(text, big_key):
-	    res = ''
-	    alph = 'abcdefghijklmnopqrstuvwxyz'
-	    i = 1
-	    for char in big_key:
-	        new = new_alph(char)
-	        for t in text:
-	            if alph.count(t) == 1 :
-	                res += new[alph.index(t)]
-	                text = text[i:]
-	                break
-	            elif alph.count(t.lower()) == 1:
-	                res += new[alph.index(t.lower())].upper()
-	                text = text[i:]
-	                break
-	            else:
-	                res += t
-	                text = text[i:]
-	                break
-	            i += 1    
-	    return res
 	def decrypt(text, big_key):
 	    res = ''
 	    alph = 'abcdefghijklmnopqrstuvwxyz'
